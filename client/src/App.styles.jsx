7c999c93449ac627f937d28cdae37e2c212f4a77
Hieu Le <hieuhmle@gmail.com>
2022-01-08 15:21:36 -0600

    separate project into clien and server

---
+++ client/src/App.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ProfilePicture = styled.img`
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  border: 1px solid grey;
+  cursor: pointer;
+`;
