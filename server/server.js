212b47e5e6950683cfc9414ef071cd0844d13d3a
Hieu Le <hieuhmle@gmail.com>
2022-01-10 23:17:30 -0600

    register user with passportjs

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const users = require("./database/users/users.utils");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Account = require("./database/accounts/accounts.model");
 // const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
@@ -14,22 +18,61 @@ const MONGO_URL =
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+// passport config
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
+  console.log("********** MongoDB connection ready! **********");
 });
 
 mongoose.connection.on("error", (error) => {
   console.error(error);
 });
 
-function startServer() {
-  mongoose.connect(MONGO_URL).then(() => {
-    app.listen(port, (error) => {
-      if (error) throw error;
-      console.log("Server running on port" + port);
-    });
+async function startServer() {
+  await mongoose.connect(MONGO_URL);
+  app.listen(port, (error) => {
+    if (error) throw error;
+    console.log("********** Server running on port " + port + " **********");
   });
 }
 
 startServer();
+
+app.get("/fetch/users", async (req, res) => {
+  users.fetchUsers(req, res);
+});
+
+app.post("/signup", (req, res) => {
+  const { email, username, name, password } = req.body;
+  Account.register(
+    new Account({ username: email }),
+    password,
+    async (error, account) => {
+      if (error) {
+        console.error(error);
+      }
+
+      const newUser = await users.createUser(
+        account._id,
+        email,
+        username,
+        name
+      );
+
+      res.send(newUser);
+    }
+  );
+});
