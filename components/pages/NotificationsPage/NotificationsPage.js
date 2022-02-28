17419e12f817e83c91ef7ddb9f27a6863b1fac2f
Hieu Le <hieuhmle@gmail.com>
2022-02-28 16:47:44 -0600

    fix margin bugs

--- components/pages/NotificationsPage/NotificationsPage.js
+++ components/pages/NotificationsPage/NotificationsPage.js
@@ -1,5 +1,15 @@
+import { Fragment } from "react";
+
 import Notifications from "../../elements/Notifications/Notifications";
 
-const NotificationsPage = () => <Notifications />;
+import styles from "./NotificationsPage.module.css";
+
+const NotificationsPage = () => (
+  <Fragment>
+    <main className={styles.container}>
+      <Notifications />
+    </main>
+  </Fragment>
+);
 
 export default NotificationsPage;
